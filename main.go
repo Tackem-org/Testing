b9efbdb71fa7e2d53998a7ac44632eb77e3e9d0b
Gary The Brown <GaryBrownSunderland@gmail.com>
2022-05-14 02:40:50 +0100

    cleanup of code

--- main.go
+++ main.go
@@ -6,44 +6,19 @@ import (
 	"github.com/Tackem-org/testing/static"
 	"github.com/Tackem-org/testing/web"
 
-	pbc "github.com/Tackem-org/Global/pb/config"
 	pbr "github.com/Tackem-org/Global/pb/registration"
 )
 
-const (
-	masterConfigFile = "test.json"
-	logFile          = "test.log"
-)
-
 var (
-	Version    string = "v0.0.0-devel"
-	Commit     string
-	CommitDate string
-	sd         = &setupData.SetupData{
-
+	Version string = "v0.0.0-devel"
+	sd             = &setupData.SetupData{
 		ServiceName: "test",
 		ServiceType: "system",
 		SingleRun:   false,
 		StartActive: true,
 		VerboseLog:  true,
-		ConfigItems: []*pbr.ConfigItem{
-			{
-				Key:          "user.password.minimum",
-				DefaultValue: "8",
-				Type:         pbc.ValueType_Uint,
-				Label:        "Password Minimum Length",
-				HelpText:     "what is the minimum password length",
-				InputType:    pbr.InputType_INumber,
-				InputAttributes: &pbr.InputAttributes{
-					Other: map[string]string{
-						"min": "1",
-						"max": "16",
-					},
-				},
-			},
-		},
 		NavItems: []*pbr.NavItem{
-			{LinkType: pbr.LinkType_User, Title: "Test", Icon: "user", Path: "/test"},
+			{LinkType: pbr.LinkType_Main, Title: "Test", Icon: "user", Path: "/"},
 		},
 		StaticFS: &static.FS,
 		Paths: []*setupData.PathItem{
@@ -57,5 +32,5 @@ var (
 )
 
 func main() {
-	system.Run(sd, masterConfigFile, logFile, Version, Commit, CommitDate)
+	system.Run(Version, sd)
 }
